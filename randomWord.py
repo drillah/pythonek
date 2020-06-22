999d7eb79a617214d630c99646208a7bad864193
Dariusz Radyszkiewicz <dariuszradyszkiewicz@gmail.com>
2020-06-23 00:35:08 +0200

    Some exercises from M. Dawson book (first bigger one)

--- randomWord.py
+++ randomWord.py
@@ -5,23 +5,32 @@ WORDS = ("zima", "wiatr", "orzech", "parawan", "towar")
 
 word = random.choice(WORDS)
 word_counter = len(word)
-
+count = 5
 print("Twoje słow składa się z", word_counter, "liter")
 info = "Podaj odpowiedź bądź litere aby sprawdzić czy znajduje się w słowie: "
 check = input(info)
 while check != word:
     if check == word:
         print("Gratulacje udało się!")
+    elif count == 0:
+        print("Niestety nie udało Ci się odgadnąć w wyznaczonej ilości prób")
+        break
     elif len(check) == 1:
         for letter in check:
-            if letter.lower() in check:
+            if letter.lower() in word:
                 print("Litera", letter, "znajduje się w słowie")
-                input(info)
+                count -= 1
+                print("Pozostało", count + 1)
+                check = input(info)
             else:
                 print("Nie ma takiej litery w słowie")
-                input(info)
+                count -= 1
+                print("Pozostało", count + 1)
+                check = input(info)
     else:
         print("To nie ten wyraz, próbuj dalej")
-        input(info)
+        count -= 1
+        print("Pozostało", count + 1)
+        check = input(info)
 else: print("Gratulacje udało się wygrać")
 input("Aby zakończyć naciśnij Enter.")
